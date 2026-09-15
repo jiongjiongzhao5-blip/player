7d369c877341d7d5492595e512839320e52dfd40
幺幺零柒 <jiongjiongzhao5@gmail.com>
2026-09-15 15:34:42 +0800

    av_utils.h：6 个 RAII 删除器 + 智能指针别名 + 版本断言

--- build/Desktop_Qt_6_7_3_MSVC2022_64bit_Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,6 +0,0 @@
-set(__QT_DEPLOY_TARGET_voice_player_learn_FILE C:/Workspace/Documents/开发/player/build/Desktop_Qt_6_7_3_MSVC2022_64bit_Debug/voice_player_learn.exe)
-set(__QT_DEPLOY_TARGET_voice_player_learn_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_voice_player_learn_RUNTIME_DLLS C:/Apps/QTnew/6.7.3/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Apps/QTnew/6.7.3/msvc2022_64/bin/Qt6Guid.dll;C:/Apps/QTnew/6.7.3/msvc2022_64/bin/Qt6Cored.dll)
-set(__QT_DEPLOY_TARGET_test_av_utils_FILE C:/Workspace/Documents/开发/player/build/Desktop_Qt_6_7_3_MSVC2022_64bit_Debug/test_av_utils.exe)
-set(__QT_DEPLOY_TARGET_test_av_utils_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_test_av_utils_RUNTIME_DLLS )
